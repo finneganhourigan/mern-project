aeda57dbd41f5423e6cf464e97ffc3df48e7f1cf
finneganhourigan <finneganhourigan@gmail.com>
2023-12-28 20:22:13 -0600

    connected to database

--- server/server.js
+++ server/server.js
@@ -4,7 +4,18 @@ require('dotenv').config();
 
 const app = express();
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("listening on port:", process.env.PORT);
-});
+//middleware
+
+//routes
+
+//connect to db (asynchronous)
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db listening on port:", process.env.PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
