c22aabaea171f79658be2bc50740245c4cb077d0
finneganhourigan <finneganhourigan@gmail.com>
2024-01-10 22:33:21 -0600

    Created css files for each component/page. Implemented bootstrap for responsive grid. Styled Disc component.

--- client/src/components/Disc.jsx
+++ client/src/components/Disc.jsx
@@ -1,16 +1,19 @@
+import '../styles/disc.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const Disc = (props) => {
     return (
         <div className="disc">
-            <h1>{props.manufacturer} {props.name}</h1>
+            <h1>{props.name}</h1>
+            <h3>{props.manufacturer}</h3>
             <img src={`/assets/${props.image}`} alt={props.name} />
             <h3>{props.category}</h3>
             <div className="flight-number-grid">
-                <p className="flight-num">Speed: {props.speed}</p>
-                <p className="flight-num">Turn: {props.turn}</p>
-                <p className="flight-num">Glide: {props.glide}</p>
-                <p className="flight-num">Fade: {props.fade}</p>
+                <p>Speed: {props.speed}</p>
+                <p>Turn: {props.turn}</p>
+                <p>Glide: {props.glide}</p>
+                <p>Fade: {props.fade}</p>
             </div>
-            {/* <p>Description: {props.description}</p> */}
         </div>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Disc from "../components/Disc";
-import '../index.css';
+import '../styles/home.css';
+
 
 const Home = () => {
     const [discs, setDiscs] = useState([]);
@@ -16,17 +17,19 @@ const Home = () => {
                 
                 setFilteredDiscs(data.map(disc => {
                     return (
-                        <Disc key={disc._id}
-                            name={disc.name}
-                            manufacturer={disc.manufacturer}
-                            speed={disc.speed}
-                            glide={disc.glide}
-                            turn={disc.turn}
-                            fade={disc.fade}
-                            category={disc.category}
-                            description={disc.description}
-                            image={disc.image}
-                        />
+                        <div className="col-sm-6 col-md-4 col-lg-3">
+                            <Disc key={disc._id}
+                                name={disc.name}
+                                manufacturer={disc.manufacturer}
+                                speed={disc.speed}
+                                glide={disc.glide}
+                                turn={disc.turn}
+                                fade={disc.fade}
+                                category={disc.category}
+                                description={disc.description}
+                                image={disc.image}
+                            />
+                        </div>
                     )
                 }));
 
@@ -40,7 +43,11 @@ const Home = () => {
     return (
         <div className="home">
             <div className="disc-grid">
-                {isLoading ? <h2>Loading Disc Data...</h2> : <>{filteredDiscs}</>}
+                <div className="container-fluid">
+                    <div className="row">
+                        {isLoading ? <h2>Loading Disc Data...</h2> : <>{filteredDiscs}</>}
+                    </div>
+                </div>
             </div>
         </div>
     )
